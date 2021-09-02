d3057e3e0a4023b8ca16345f2f5fc4db3fe35b7c
Kyle Mathews <mathews.kyle@gmail.com>
2021-09-01 18:52:14 -0700

    Add a source node cache & change both to incrementing integers

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,39 @@
-exports.sourceNodes = async ({cache}) => {
-  const prevValue = await cache.get(`hi`)
+exports.sourceNodes = async ({ cache, actions, getNode }) => {
+  // Create/update plugin cache value
+  let prevValue = (await cache.get(`hi`)) || 0
   if (!prevValue) {
     console.log(`cache doesn't exist`)
   } else {
     console.log(`cache does exist`)
-    console.log({prevValue})
+    console.log({ prevValue })
   }
 
-  await cache.set(`hi`, Date.now())
+  prevValue += 1
+  await cache.set(`hi`, prevValue)
 
+  let node = getNode(`test-cache`)
+
+  let nodeCounter = 0
+  if (node) {
+    console.log(`before`, { node })
+    nodeCounter = node.nodeCounter + 1
+  }
+
+  console.log({ nodeCounter: node?.nodeCounter })
+  console.log({ nodeCounter })
+
+  actions.createNode({
+    id: `test-cache`,
+    parent: null,
+    children: [],
+    nodeCounter,
+    internal: {
+      contentDigest: nodeCounter.toString(),
+      type: `testCache`,
+    },
+  })
+
+  node = getNode(`test-cache`)
+
+  console.log(`after`, { node })
 }
